f73cfa78f93a5263b3e9ee6e1dd1985ab26e98d8
Karri Folan Di Petrillo <karri.folan.di.petrillo@cern.ch>
2019-12-12 16:43:19 -0600

    adding argparsing

--- read_resistance.py
+++ read_resistance.py
@@ -1,21 +1,35 @@
+import argparse
 import pyfirmata
 import time
 
-#Can use Arduino IDE to find board's location
-board = pyfirmata.Arduino('/dev/cu.usbmodem1421')
+
+# *
+# Argument parsing
+# *
+parser = argparse.ArgumentParser(description="Temperature measurement with ArduinoUno voltage divider")
+parser.add_argument('-o' , '--output_directory'            , dest='output_directory'     , help='directory for saving text files with temp'              , required=False   , default = 'output' )
+parser.add_argument('-b' , '--board_location'              , dest='board_location'       , help='location of ArduinoUno, usually "/dev/xxxx"'            , required=False   , default='/dev/cu.usbmodem1421' )
+parser.add_argument('-p' , '--analog_pin'                  , dest='analog_pin'           , help='analog pin which reads voltage across thermister, int'  , required=False   , default = 1    )           
+parser.add_argument('-n' , '--num_samples'                 , dest='num_samples'          , help='number of samples per ADC measurement'                  , required=False   , default = 10   )
+parser.add_argument('-r' , '--series_resistance'           , dest='series_resistance'    , help='value of resistor in series with thermistor, in ohms'   , required=False   , default = 4700 )
+
+args = parser.parse_args()
+
+output_directory  = args.output_directory
+board_location    = args.board_location #can be found from Arduino IDE which scans USB ports
+analog_pin        = args.analog_pin
+num_samples       = args.num_samples
+series_resistance = args.series_resistance
+
+# * 
+# Setup board and analog pin
+# * 
+board = pyfirmata.Arduino(board_location)
 
 it = pyfirmata.util.Iterator(board)
 it.start()
 
-##
-## Initialize important values
-##
-analogPin = 1  # replace with analog pin
-adcMax = 1023.00 # Arduino uses 10-bit ADC 
-numSamples = 10
-R0 = 4700. # Resistor in series = 4.7 kOhm 
-
-analog_input = board.get_pin('a:1:i')
+analog_input = board.get_pin('a:{}:i'.format(analog_pin))
   
 def getResistance(): 
 
@@ -23,17 +37,18 @@ def getResistance():
   # Read ADC Value for Analog Pin, sample multiple times
   # *
   adcVal = 0
-  for i in range (0,numSamples):
+  for i in range (0,num_samples):
      adcVal = adcVal + analog_input.read() 
      time.sleep(0.1)
 
-  adcVal = adcVal/numSamples
-  
+  adcVal = adcVal/num_samples
+  # note: in pyfirmata reading analog pins returns a decimal between 0 and 1, where 1 is max value 
+ 
   # * 
   # Compute resistance 
   # * 
  
-  resist = R0 * ( 1.0 / float(adcVal) - 1.00 ) 
+  resist = series_resistance * ( 1.0 / float(adcVal) - 1.00 ) 
 
   return resist
 
